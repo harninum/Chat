bfad29de2b0d212fbd283c6184448e1d02770fcc
harninum <70820319+harninum@users.noreply.github.com>
2021-01-02 22:04:22 +0300

    Add files via upload

---
+++ server/Server/Controllers/chatController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class chatController : ControllerBase
+    {
+        static MessagesClass ms = new MessagesClass();
+
+        // GET api/<chatController>/5
+        [HttpGet("{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public ActionResult<string> Get(int id)
+        {
+            string json = "Not found";
+            if ((id < ms.GetCountMessages()) && (id >= 0))
+            {
+                json = JsonSerializer.Serialize(ms.Get(id));
+                return json.ToString();
+            }
+            return NotFound();
+        }
+
+        // POST api/<chatController>
+        [HttpPost]
+        public void Post([FromBody] message msg)
+        {
+            ms.Add(msg);
+            Console.WriteLine($"{msg.username}:  {msg.text} ({ms.messages.Count})");
+        }
+    }
+}
---
+++ server/Server/MesClass.cs
@@ -0,0 +1,87 @@
+﻿using Server;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Server
+{
+    [Serializable]
+    public class message
+    {
+        public string username { get; set; }
+        public string text { get; set; }
+        public DateTime timestamp { get; set; }
+        public message()
+        {
+            this.username = "Server";
+            this.text = "Server is running...";
+            this.timestamp = DateTime.UtcNow;
+        }
+
+        public message(string username, string text)
+        {
+            this.username = username;
+            this.text = text;
+            this.timestamp = DateTime.UtcNow;
+        }
+    }
+[Serializable]
+public class MessagesClass
+{
+    public List<message> messages = new List<message>();
+
+    public void Add(message ms)
+    {
+        ms.timestamp = DateTime.UtcNow;
+        messages.Add(ms);
+        Console.WriteLine(messages.Count);
+    }
+
+    public void Add(string username, string text)
+    {
+        message msg = new message(username, text);
+        messages.Add(msg);
+        Console.WriteLine(messages.Count);
+    }
+
+    public message Get(int id)
+    {
+            return messages.ElementAt(id);
+    }
+
+
+    public int GetCountMessages()
+    {
+        return messages.Count;
+    }
+
+
+    public MessagesClass()
+    {
+        messages.Clear();
+        message ms = new message();
+        messages.Add(ms);
+    }
+
+    public void SaveData()
+    {
+            Console.WriteLine("Save messages");
+            // сохранение данных
+            using (FileStream fs = new FileStream("server_data.json", FileMode.OpenOrCreate))
+            {
+                JsonSerializer.SerializeAsync<MessagesClass>(fs, this);
+                Console.WriteLine("Data has been saved to file");
+            }
+    }
+
+    public override string ToString()
+    {
+        return messages.ToString();
+    }
+}
+
+
+}
